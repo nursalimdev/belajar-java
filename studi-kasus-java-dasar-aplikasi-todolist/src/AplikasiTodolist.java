202d73986d33bb5caaeb2a15d47f1cb8247f7384
Nursalim <nursalimdev123@gmail.com>
2023-06-29 21:38:58 +0700

    commit

---
+++ studi-kasus-java-dasar-aplikasi-todolist/src/AplikasiTodolist.java
@@ -0,0 +1,27 @@
+public class AplikasiTodolist {
+    public static String[] model = new String[10];
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Menampilkan todolist
+     */
+    public static void showTodolist() {
+
+    }
+
+    /**
+     * Menambah todolist
+     */
+    public static void addTodolist() {
+
+    }
+
+    /**
+     * Menghapus todolist
+     */
+    public static void removeTodolist() {
+
+    }
+}
