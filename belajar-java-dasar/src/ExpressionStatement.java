901115281a3876e4b65f6bb49472acb89aabba39
Nursalim <nursalimdev123@gmail.com>
2023-06-29 14:54:28 +0700

    commit

---
+++ belajar-java-dasar/src/ExpressionStatement.java
@@ -0,0 +1,12 @@
+public class ExpressionStatement {
+    public static void main(String[] args) {
+        // expression
+        int value;
+
+        // expression
+        value = 10;
+
+        // statement
+        System.out.println(value);
+    }
+}
---
+++ belajar-java-dasar/src/IfElseStatement.java
@@ -0,0 +1,12 @@
+public class IfElseStatement {
+    public static void main(String[] args) {
+        var nilai = 80;
+        var absen = 90;
+
+        if(nilai >= 75 && absen >= 75){
+            System.out.println("Lulus");
+        }else{
+            System.out.println("Anda gagal");
+        }
+    }
+}
